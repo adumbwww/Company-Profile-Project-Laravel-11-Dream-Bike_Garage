1925593484c5673f424d9d1fcacf1a1689e76ebb
Adumb19 <adamwidianto19@gmail.com>
2024-06-11 14:02:08 +0700

    .

--- resources/views/home/inc/head.blade.php
+++ resources/views/home/inc/head.blade.php
@@ -4,12 +4,12 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Dream Bike Motorcycle</title>
+  <title>Dream Bike Garage</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('Gp/assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('favicon.ico')}}" rel="icon">
   <link href="{{asset('Gp/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Fonts -->
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -264,37 +264,26 @@
           </script>
           <div class="swiper-wrapper">
 
+            @foreach ($testimonial as $testi)
             <div class="swiper-slide">
               <div class="testimonial-item">
-                <img src="{{ asset('Gp/assets/img/testimonials/testimonials-1.jpg') }}" class="testimonial-img" alt="">
-                <h3>Saul Goodman</h3>
+                <img src="{{ asset('Gp/assets/img/testimonials/testimonials-1(deletethis).jpg') }}" class="testimonial-img" alt="">
+                <h3>
+                  {{ $testi->name }}
+                </h3>
                 <h4>Ceo &amp; Founder</h4>
                 <div class="stars">
                   <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
                 </div>
                 <p>
                   <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.</span>
+                  <span> {{ $testi->testimonial }}
+                  </span>
                   <i class="bi bi-quote quote-icon-right"></i>
                 </p>
               </div>
             </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-item">
-                <img src="{{ asset('Gp/assets/img/testimonials/testimonials-2.jpg') }}" class="testimonial-img" alt="">
-                <h3>Sara Wilsson</h3>
-                <h4>Designer</h4>
-                <div class="stars">
-                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
-                </div>
-                <p>
-                  <i class="bi bi-quote quote-icon-left"></i>
-                  <span>Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.</span>
-                  <i class="bi bi-quote quote-icon-right"></i>
-                </p>
-              </div>
-            </div><!-- End testimonial item -->
+            @endforeach
 
           </div>
           <div class="swiper-pagination"></div>
